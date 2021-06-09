b9b52776911173e1678633e839ef73112266b31a
Ahad <ahadmoideen@gmail.com>
2021-06-09 01:46:14 -0700

    add root dir

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 sudo ln -s /bin/sh /bin/sh^M
+cd /root/
 pip3 install -r requirements.txt
 python manage.py makemigrations && python manage.py migrate
 nohup python manage.py runserver 0.0.0.0:8000 &
