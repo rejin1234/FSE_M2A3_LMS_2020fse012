9385a6145a3170063f52228abcec85f16dffb7c2
Ahad <ahadmoideen@gmail.com>
2021-06-09 01:40:10 -0700

    sudo ln

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+sudo ln -s /bin/sh /bin/sh^M
 pip3 install -r requirements.txt
 python manage.py makemigrations && python manage.py migrate
 nohup python manage.py runserver 0.0.0.0:8000 &
